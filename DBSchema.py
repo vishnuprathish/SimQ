d10d5aa7b4f51a807bca4432dddfbd2b3b99207e
Vishnu Prathish <vishnu.prathish@gmail.com>
2014-03-31 04:20:33 -0400

    savign major changes

--- DBSchema.py
+++ DBSchema.py
@@ -11,20 +11,22 @@ class Relation:
     size=0
     
     TableData={
-        'A':[x for x in range(1000)]
-        'B':random.shuffle([x/3 for x in range(1000)])
-        'C':random.shuffle([x/6 for x in range(1000)])
+        'A':list([x for x in range(100)]),
+        'B':list([random.randint(0,x) for x in range(0,100)]),
+        'C':list([random.randint(0,x) for x in range(0,100)])
     }
 
     
-    def __init__(self, tableName,columns=None,pKey=None):
+    def __init__(self, tableName,columns,pKey=None):
         self.tableName=tableName
         self.pKey=pKey
         self.columns=columns
+        self.runs=[0 for x in range(len(self.columns))]
         pass
     
     def __str__(self):
-        return str(self.size)
+        
+        return str("Size: " + str(self.size) + "  Runs: "+ str(self.runs)) #+ str(self.TableData))
         
     def setPkey(self,pKey):
         self.pKey=pKey
@@ -57,13 +59,23 @@ class Relation:
         self.uKeys=uKeys
     
     def setSize(self,size):
-        self.size=size
+        self.size=int(size)
     
     def getRuns(self):
         return self.runs
+        
+    def getColRuns(self,colName):  #get the number of runs of a specified columns
+        print self.getColIdx(colName)
+        return self.runs[self.getColIdx(colName)]
     
     def getSize(self):
         return self.size
+        
+    def getMaxT(self,columnName):
+        return max(self.TableData[columnName])
+        
+    def getMinT(self,columnName):
+        return min(self.TableData[columnName])
         
     def getColIdx(self,columnName):
         i=0
@@ -92,18 +104,51 @@ class Relation:
         return x
         pass
         
-    def select(self,args):
-        x=Relation("result",self.columns+s.getColumns(),None)
-        x.setStats(self,None,None,self.size*S.getSize())
+    def getSelFactor(self,args):
         if(args[1]=='='):
-            if(args[2] in self.getcolumnNames()):
+            selFactor=float(1/float(self.ukeys[self.getColIdx(args[0])]))
+        elif(args[1]=='>'):      #Coulmn > Value
+            selfFactor = float(((self.getMaxT(args[0])) - int(args[2]))) / float(( self.getMaxT(args[0]) - self.getMinT(args[0]) ))
+        elif(args[1]=='<'):      #Coulmn > Value
+            selfFactor = float(((self.getMaxT(args[0])) - int(args[2]))) / float(( self.getMaxT(args[0]) - self.getMinT(args[0]) ))
+        elif(args[1]=='IN'):
+            factors=[self.getSelFactor([args[0],'=',value]) for value in args[2]]
+            final=factors[0]
+            del factors[0]
+
+            for smrit in factors:
+                final=float(float(final) * float(smrit))
+            selFactor=final
+        return selFactor
+        
+    def select(self,args):
+        x=Relation("result",self.columns,None)
+        #x.setStats(self,None,None,self.size*S.getSize())
+        if(args[1]=='='):
+            if(args[2] in self.getColumnNames()):
                 x.setStats()
-                pass
+                pass    
             else:
-                x.setSize(self.size/self.uKeys[getColIdx[args[2]]])  #Column = value 
+                sFactor=self.getSelFactor(args)
+                x.setSize(self.size * sFactor)  #Column = value 
+                
+                #finding the number of runs for Column = value predicate. 
+                chRuns = [self.getColRuns(args[0])*sFactor,self.getColRuns(args[0])*sFactor , self.getColRuns(args[0])*sFactor]
+                chRuns[self.getColIdx(args[0])]=1
+                print chRuns
+                x.setRuns(chRuns)
                 pass 
         
         if(args[1]=='>'):      #Coulmn > Value
+            selfFactor = float(((self.getMaxT(args[0])) - int(args[2]))) / float(( self.getMaxT(args[0]) - self.getMinT(args[0]) ))
+            x.setSize(selfFactor * self.size) 
+            
+        elif(args[1]=='<'):      #Coulmn > Value
+            selfFactor = float(((self.getMaxT(args[0])) - int(args[2]))) / float(( self.getMaxT(args[0]) - self.getMinT(args[0]) ))
+            x.setSize((1-selfFactor) * self.size)  
+        elif(args[1]=='IN'):
+            selfFactor=self.getSelFactor(args)
+            x.setSize(selfFactor * self.size)
             
             
         return x
@@ -126,12 +171,23 @@ class Relation:
 
 
 R = Relation("R",{'A':'int','B':'int','C':'int'})
-R.setStats([100],100,1000)
+R.setStats([100],[100,100,300],100)
 print R.getColumnNames()
-S = Relation("R")
 S = Relation("R",{'A':'int','B':'int','D':'int'})
-S.setStats([100],100,1000)
+S.setStats([100],[100,2,300],100)
 print S.getColumnNames()
 
-print R.union(S)
+print S.select(['A','>','25'])
+print S.select(['B','>','25'])
+print S.select(['C','>','25'])
+
+print S.select(['A','<','25'])
+print S.select(['A','<','75'])
+print S.select(['B','<','25'])
+
+print S.select(['B','=','25'])
+
+print S.select(['B','IN',[10,20,30]])
+
+
 
