909516fba4b5b1b1e3974596e0f44da175c38ead
Vishnu Prathish <vishnu.prathish@gmail.com>
2014-04-01 09:10:01 -0400

    added cross product,union , intersection adn disjoin operator

--- DBSchema.py
+++ DBSchema.py
@@ -27,6 +27,11 @@ class Relation:
     def __str__(self):
         
         return str("Size: " + str(self.size) + "  Runs: "+ str(self.runs)) #+ str(self.TableData))
+
+    def setStats(self,runs=None,ukeys=None,size=None):
+        self.runs=runs
+        self.ukeys=ukeys
+        self.size=size
         
     def setPkey(self,pKey):
         self.pKey=pKey
@@ -44,14 +49,15 @@ class Relation:
         
     def getColumns(self):
         return self.columns
+
+    #def getColumnFromName(self,colName):
+        #return self.columns
         
     def getColumnNames(self):
         return [x for x in self.columns.keys()]
 
-    def setStats(self,runs=None,ukeys=None,size=None):
-        self.runs=runs
-        self.ukeys=ukeys
-        self.size=size
+    
+        
     def setRuns(self,runs):
         self.runs=runs
     
@@ -59,13 +65,14 @@ class Relation:
         self.uKeys=uKeys
     
     def setSize(self,size):
-        self.size=int(size)
+        self.size=size
     
     def getRuns(self):
         return self.runs
         
     def getColRuns(self,colName):  #get the number of runs of a specified columns
-        print self.getColIdx(colName)
+        #print self.getColIdx(colName)
+        #print self.runs
         return self.runs[self.getColIdx(colName)]
     
     def getSize(self):
@@ -76,31 +83,42 @@ class Relation:
         
     def getMinT(self,columnName):
         return min(self.TableData[columnName])
+
+    def getColumnsWithTableName(self):   #return table names as A.columname format
+        return [self.getName()+'.'+key for key in self.columns.keys()]
         
     def getColIdx(self,columnName):
         i=0
         for x in self.getColumnNames():       
             if columnName==x:
-                return i
+                return int(i)
             i+=1
         return -1
         
     def union(self,S):
         x=Relation("result",self.columns,self.pKey)
-        x.setStats(self.runs+S.getRuns(),None,self.size+S.getSize())
+        x.setStats([z+y for (z,y) in zip(self.runs,S.getRuns())],[],self.size+S.getSize())
         return x
         pass
 
-    def intersect(self,S):
+    def disjoin(self,S):        # CHECK : is N(R) x-y for disjoin?? 
         x=Relation("result",self.columns,self.pKey)
-        x.setStats(self,None,None,self.size-S.getSize())
+        x.setStats([z-y for (z,y) in zip(self.runs,S.getRuns())],None,self.size-S.getSize())
+        return x
+        pass
+
+    def intersect(self,S):     # TODO: Compute stats
+        x=Relation("result",self.columns,self.pKey)
+        x.setStats([z-y for (z,y) in zip(self.runs,S.getRuns())],None,self.size-S.getSize())
         return x
         pass
         
-    def cartProd(self,S):
+    def crossProd(self,S):       #TODO: Compute Stats
         
-        x=Relation("result",self.columns+S.getColumns(),None)
-        x.setStats(self,None,None,self.size*S.getSize())
+        x=Relation("result",self.getColumnsWithTableName()+S.getColumnsWithTableName(),None)
+        x.setStats(None,None,self.size*S.getSize())
+        chRuns = [self.getSize()*S.runs[u] for u in range(len(self.runs))]     #Not accurate. Have to take care of additional runs equal to the number of runs in outer if there is a 'down' between first and last element in the list. 
+        x.setRuns(chRuns)
         return x
         pass
         
@@ -109,7 +127,7 @@ class Relation:
             selFactor=float(1/float(self.ukeys[self.getColIdx(args[0])]))
         elif(args[1]=='>'):      #Coulmn > Value
             selfFactor = float(((self.getMaxT(args[0])) - int(args[2]))) / float(( self.getMaxT(args[0]) - self.getMinT(args[0]) ))
-        elif(args[1]=='<'):      #Coulmn > Value
+        elif(args[1]=='<'):      #Coulmn > value
             selfFactor = float(((self.getMaxT(args[0])) - int(args[2]))) / float(( self.getMaxT(args[0]) - self.getMinT(args[0]) ))
         elif(args[1]=='IN'):
             factors=[self.getSelFactor([args[0],'=',value]) for value in args[2]]
@@ -127,25 +145,35 @@ class Relation:
         if(args[1]=='='):
             if(args[2] in self.getColumnNames()):
                 x.setStats()
-                pass    
+                pass
             else:
+                
                 sFactor=self.getSelFactor(args)
+
                 x.setSize(self.size * sFactor)  #Column = value 
                 
                 #finding the number of runs for Column = value predicate. 
-                chRuns = [self.getColRuns(args[0])*sFactor,self.getColRuns(args[0])*sFactor , self.getColRuns(args[0])*sFactor]
+                #chRuns = [self.getColRuns(args[0])*sFactor,self.getColRuns(args[0])*sFactor , self.getColRuns(args[0])*sFactor]
+                chRuns=[self.runs[u]*sFactor for u in range(len(self.runs))]
                 chRuns[self.getColIdx(args[0])]=1
-                print chRuns
+
                 x.setRuns(chRuns)
                 pass 
         
-        if(args[1]=='>'):      #Coulmn > Value
+        elif(args[1]=='>'):      #Coulmn > Value
             selfFactor = float(((self.getMaxT(args[0])) - int(args[2]))) / float(( self.getMaxT(args[0]) - self.getMinT(args[0]) ))
-            x.setSize(selfFactor * self.size) 
+            x.setSize(selfFactor * self.size)
+
+            chRuns=[self.runs[u]*selfFactor for u in range(len(self.runs))]
+            x.setRuns(chRuns) 
             
-        elif(args[1]=='<'):      #Coulmn > Value
+        elif(args[1]=='<'):      #Coulmn < Value
             selfFactor = float(((self.getMaxT(args[0])) - int(args[2]))) / float(( self.getMaxT(args[0]) - self.getMinT(args[0]) ))
             x.setSize((1-selfFactor) * self.size)  
+
+            chRuns=[self.runs[u]*(1-selfFactor) for u in range(len(self.runs))]
+            x.setRuns(chRuns) 
+
         elif(args[1]=='IN'):
             selfFactor=self.getSelFactor(args)
             x.setSize(selfFactor * self.size)
@@ -171,11 +199,13 @@ class Relation:
 
 
 R = Relation("R",{'A':'int','B':'int','C':'int'})
-R.setStats([100],[100,100,300],100)
-print R.getColumnNames()
+R.setStats([10,20,30],[100,100,300],100)
+
+
+#print R.getColumnNames()
 S = Relation("R",{'A':'int','B':'int','D':'int'})
-S.setStats([100],[100,2,300],100)
-print S.getColumnNames()
+S.setStats([20,50,25],[100,200,300],100)
+'''print S.getColumnNames()
 
 print S.select(['A','>','25'])
 print S.select(['B','>','25'])
@@ -185,9 +215,15 @@ print S.select(['A','<','25'])
 print S.select(['A','<','75'])
 print S.select(['B','<','25'])
 
-print S.select(['B','=','25'])
+print S.select(['B','=','50'])'''
 
-print S.select(['B','IN',[10,20,30]])
+print S
+print R
+#print S.union(R)
+
+print R.crossProd(S)
+
+#print S.select(['B','IN',[10,20,30]])
 
 
 
